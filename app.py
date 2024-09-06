25e5f3f9519817d081d09fc06d29ed094ca0f0ab
Abhigyan ranjan <114335049+Abhigyan-RA@users.noreply.github.com>
2024-09-06 17:59:49 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -1,41 +1,54 @@
-from sklearn.preprocessing import LabelEncoder
 from fastapi import FastAPI
 from pydantic import BaseModel
-import pickle
+import joblib
 import numpy as np
 
 app = FastAPI()
 
 
-with open('model.pkl', 'rb') as model_file:
-    model = pickle.load(model_file)
-
-with open('scaler.pkl', 'rb') as scaler_file:
-    scaler = pickle.load(scaler_file)
+model = joblib.load('model.pkl')
+scaler = joblib.load('scaler.pkl')
+label_encoder = joblib.load('label_encoder.pkl')
 
 
-with open('label_encoder.pkl', 'rb') as le_file:
-    le = pickle.load(le_file)
-
-class CropPredictionRequest(BaseModel):
+class PredictionRequest(BaseModel):
     temperature: float
     humidity: float
     rainfall: float
+    label: str
 
-@app.post('/predict')
-def predict_crop(request: CropPredictionRequest):
-    
-    data = np.array([[request.temperature, request.humidity, request.rainfall]])
-    data = scaler.transform(data)
-    
 
-    prediction = model.predict(data)[0]
-    
-    
-    crop = le.inverse_transform([prediction])[0]
-    
-    return {'predicted_crop': crop}
+@app.get("/")
+def read_root():
+    return {"message": "Welcome to the NPK Prediction API"}
 
-if __name__ == '__main__':
-    uvicorn.run(app, host='127.0.0.1', port=8000)
+
+@app.post("/predict/")
+def predict_npk(data: PredictionRequest):
+    
+    temperature = data.temperature
+    humidity = data.humidity
+    rainfall = data.rainfall
+    label = data.label
+
+    
+    label_encoded = label_encoder.transform([label])[0]
+    numerical_input_data = np.array([[temperature, humidity, rainfall]])
+
+    scaled_numerical_data = scaler.transform(numerical_input_data)
+
+    input_data = np.concatenate([scaled_numerical_data, np.array([[label_encoded]])], axis=1)
+
+    prediction = model.predict(input_data)
+
+   
+    return {
+        "N": prediction[0][0],
+        "P": prediction[0][1],
+        "K": prediction[0][2]
+    }
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=8000)
 
